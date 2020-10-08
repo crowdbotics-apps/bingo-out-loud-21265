5a655d61e5d6970c364d11ddbb6227b8354d9eb1
Crowdbotics <team@crowdbotics.com>
2020-10-08 18:02:05 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/bingo_out_loud_21265/MainActivity.java
@@ -0,0 +1,15 @@
+package com.bingo_out_loud_21265;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+    * Returns the name of the main component registered from JavaScript. This is used to schedule
+    * rendering of the component.
+    */
+    @Override
+    protected String getMainComponentName() {
+        return "bingo_out_loud_21265";
+    }
+}
